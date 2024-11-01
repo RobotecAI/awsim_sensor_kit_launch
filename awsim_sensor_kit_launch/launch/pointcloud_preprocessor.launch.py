931930c3dc511750994f34d8d4824c8ea4bd1767
SakodaShintaro <shintaro.sakoda@tier4.jp>
2024-11-01 09:04:06 +0900

    fix: fix to use sample sensor kit (#22)
    * Fixed to use sample_sensor_kit
    
    Signed-off-by: Shintaro Sakoda <shintaro.sakoda@tier4.jp>
    
    * Removed common_awsim_sensor_launch
    
    Signed-off-by: Shintaro Sakoda <shintaro.sakoda@tier4.jp>
    
    * Added a trailing break line
    
    Signed-off-by: Shintaro Sakoda <shintaro.sakoda@tier4.jp>
    
    ---------
    
    Signed-off-by: Shintaro Sakoda <shintaro.sakoda@tier4.jp>

--- awsim_sensor_kit_launch/launch/pointcloud_preprocessor.launch.py
+++ awsim_sensor_kit_launch/launch/pointcloud_preprocessor.launch.py
@@ -20,35 +20,38 @@ from launch.actions import SetLaunchConfiguration
 from launch.conditions import IfCondition
 from launch.conditions import UnlessCondition
 from launch.substitutions import LaunchConfiguration
-from launch_ros.actions import ComposableNodeContainer
 from launch_ros.actions import LoadComposableNodes
 from launch_ros.descriptions import ComposableNode
 
 
 def launch_setup(context, *args, **kwargs):
-
     # set concat filter as a component
     concat_component = ComposableNode(
         package="autoware_pointcloud_preprocessor",
         plugin="autoware::pointcloud_preprocessor::PointCloudConcatenateDataSynchronizerComponent",
         name="concatenate_data",
         remappings=[
-            ("~/input/twist", "/sensing/vehicle_velocity_converter/twist_with_covariance"),
+            (
+                "~/input/twist",
+                "/sensing/vehicle_velocity_converter/twist_with_covariance",
+            ),
             ("output", "concatenated/pointcloud"),
         ],
         parameters=[
             {
                 "input_topics": [
-                    "/sensing/lidar/top/pointcloud",
-                    "/sensing/lidar/left/pointcloud",
-                    "/sensing/lidar/right/pointcloud",
+                    "/sensing/lidar/top/pointcloud_before_sync",
+                    "/sensing/lidar/left/pointcloud_before_sync",
+                    "/sensing/lidar/right/pointcloud_before_sync",
                 ],
                 "output_frame": LaunchConfiguration("base_frame"),
-                "timeout_sec": 0.01,
                 "input_twist_topic_type": "twist",
+                "publish_synchronized_pointcloud": True,
             }
         ],
-        extra_arguments=[{"use_intra_process_comms": LaunchConfiguration("use_intra_process")}],
+        extra_arguments=[
+            {"use_intra_process_comms": LaunchConfiguration("use_intra_process")}
+        ],
     )
 
     # load concat or passthrough filter
@@ -62,11 +65,12 @@ def launch_setup(context, *args, **kwargs):
 
 
 def generate_launch_description():
-
     launch_arguments = []
 
     def add_launch_arg(name: str, default_value=None):
-        launch_arguments.append(DeclareLaunchArgument(name, default_value=default_value))
+        launch_arguments.append(
+            DeclareLaunchArgument(name, default_value=default_value)
+        )
 
     add_launch_arg("base_frame", "base_link")
     add_launch_arg("use_multithread", "False")
